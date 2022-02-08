88001507db99f3dc796a4f72e3e52db89689ca19
JustInfLOL <1209936500@qq.com>
2022-02-08 11:59:28 +0800

    lib/lzo: tidy-up ifdefs Patch series "lib/lzo: performance improvements", v5.
    This patch (of 3):
    
    Modify the ifdefs in lzodefs.h to be more consistent with normal kernel
    macros (e.g., change __aarch64__ to CONFIG_ARM64).
    
    Link: http://lkml.kernel.org/r/20190205141950.9058-2-dave.rodgman@arm.com
    Signed-off-by: Dave Rodgman <dave.rodgman@arm.com>
    Cc: Herbert Xu <herbert@gondor.apana.org.au>
    Cc: David S. Miller <davem@davemloft.net>
    Cc: Nitin Gupta <nitingupta910@gmail.com>
    Cc: Richard Purdie <rpurdie@openedhand.com>
    Cc: Markus F.X.J. Oberhumer <markus@oberhumer.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Sergey Senozhatsky <sergey.senozhatsky.work@gmail.com>
    Cc: Sonny Rao <sonnyrao@google.com>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Matt Sealey <matt.sealey@arm.com>
    Cc: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Ratoriku <a1063021545@gmail.com>
    Signed-off-by: KyuoFoxHuyu <88403766@qq.com>

--- lib/lzo/lzodefs.h
+++ lib/lzo/lzodefs.h
@@ -36,7 +36,7 @@
 #define LZO_FAST_64BIT_MEMORY_ACCESS
 #elif defined(CONFIG_X86) || defined(CONFIG_PPC)
 #define LZO_USE_CTZ32	1
-#elif defined(__arm__) && (__LINUX_ARM_ARCH__ >= 5)
+#elif defined(CONFIG_ARM) && (__LINUX_ARM_ARCH__ >= 5)
 #define LZO_USE_CTZ32	1
 #endif
 
